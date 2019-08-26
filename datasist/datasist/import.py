7588041897dd87dce833f51bda416fbe7b449bbc
Dehbaiyor <44922200+Dehbaiyor@users.noreply.github.com>
2019-08-26 17:08:53 +0100

    Update import.py

--- datasist/datasist/import.py
+++ datasist/datasist/import.py
@@ -6,4 +6,5 @@ def libaries():
         from sklearn.preprocessing import train_test_split
         from sklearn.metrics import accuracy_score,auc,classification_report,confusion_matrix,f1_score,log_loss,precision_score,recall_score
         from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_squared_log_error,r2_score
+        from sklearn.model_selection import KFold, TimeSeriesSplit,StratifiedKFold,cross_val_score,GridSearchCv,RandomizedSearchCV
   
