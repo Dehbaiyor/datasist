3fb0b3abb9b43e096da0462afc55c24f3fb19941
Dehbaiyor <44922200+Dehbaiyor@users.noreply.github.com>
2019-08-26 16:51:38 +0100

    Create import.py

---
+++ datasist/datasist/import.py
@@ -0,0 +1,7 @@
+def libaries():
+        import pandas as pd
+        import numpy as np
+        import matplotlib.pylab as plt
+        import seaborn as sns
+        from sklearn.preprocessing import train_test_split
+  
