0938ac0dd474c412491c51d291948308bd36a9ff
Dehbaiyor <44922200+Dehbaiyor@users.noreply.github.com>
2019-08-26 17:03:19 +0100

    Update import.py

--- datasist/datasist/import.py
+++ datasist/datasist/import.py
@@ -4,4 +4,6 @@ def libaries():
         import matplotlib.pylab as plt
         import seaborn as sns
         from sklearn.preprocessing import train_test_split
+        from sklearn.metrics import accuracy_score,auc,classification_report,confusion_matrix,f1_score,log_loss,precision_score,recall_score
+        from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_squared_log_error,r2_score
   
